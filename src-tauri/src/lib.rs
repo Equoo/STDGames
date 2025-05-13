4ba5d27aacc3ff00fad16086eaa0074ef7dbf54c
dderny <david.derny@ik.me>
2025-05-13 23:25:28 +0200

    multithreading

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,8 +3,12 @@ pub mod game_library;
 pub mod launcher;
 pub mod desktop_icons;
 
+use serde::Deserialize;
+use serde::Serialize;
 use tauri::async_runtime::spawn;
+use tauri::{AppHandle, Manager, State};
 use once_cell::sync::Lazy;
+use tokio::sync::Mutex;
 use utils::copy_recursively;
 use std::env;
 use std::path::Path;
@@ -40,7 +44,20 @@ fn get_game_library() -> Arc<GameLibrary> {
     _launcher.library.clone()
 }
 
+#[derive(Debug, Deserialize, Serialize, Clone)]
+struct SetupState {
+    progress: u8,
+    finish: bool,
+}
+
 async fn setup(app: tauri::AppHandle) {
+	let state = app.state::<Mutex<SetupState>>();
+	let mut state_lock = state.lock().await;
+	state_lock.progress = 0;
+	state_lock.finish = false;
+	drop(state_lock);
+	println!("Lancement de la configuration...");
+
 	let user = env::var("USER").unwrap_or("".to_string());
 	fs::create_dir_all(format!("/sgoinfre/{user}/.stdgames_saves/"))
 		.expect("Erreur lors de la création du répertoire .stdgames_saves");
@@ -48,6 +65,9 @@ async fn setup(app: tauri::AppHandle) {
 	let junest_dst = format!("/goinfre/{user}/.stdgames/junest");
 	copy_recursively(Path::new("/sgoinfre/stdgames/.data/junest"), Path::new(&junest_dst)).await
 		.expect("Erreur lors de la copie du répertoire .junest");
+	let mut state_lock = state.lock().await;
+	state_lock.progress = 60;
+	drop(state_lock);
 
 	let umu_path = format!("/goinfre/{user}/.stdgames/umu");
 	if !Path::new(&umu_path).exists() {
@@ -55,6 +75,9 @@ async fn setup(app: tauri::AppHandle) {
 		let umu_dst_zip = format!("/goinfre/{user}/.stdgames/umu.zip");
 		copy_recursively(Path::new("/sgoinfre/stdgames/.data/umu.zip"), Path::new(&umu_dst_zip)).await
 			.expect("Erreur lors de la copie de umu.zip");
+		let mut state_lock = state.lock().await;
+		state_lock.progress = 75;
+		drop(state_lock);
 
 		let umu_zip_path = Path::new(&umu_dst_zip);
 		let umu_dst = format!("/goinfre/{user}/.stdgames/");
@@ -68,13 +91,30 @@ async fn setup(app: tauri::AppHandle) {
 				.expect("Erreur lors de l'extraction de umu.zip");
 		}
 	}
+	let mut state_lock = state.lock().await;
+	state_lock.progress = 100;
+	drop(state_lock);
+}
+
+#[tauri::command]
+async fn get_setup_state(state: State<'_, Mutex<SetupState>>) -> Result<SetupState, ()>	{
+	let state = state.lock().await;
+	println!("get_setup_state: {:?}", state);
+	Ok(SetupState {
+		progress: state.progress,
+		finish: state.finish,
+	})
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {	
 	tauri::Builder::default()
 		.plugin(tauri_plugin_opener::init())
-		.invoke_handler(tauri::generate_handler![game_state, get_game_library, launch_game, add_launcher_desktop_icon])
+		.manage(Mutex::new(SetupState {
+            progress: 0,
+            finish: false,
+        }))
+		.invoke_handler(tauri::generate_handler![game_state, get_game_library, launch_game, add_launcher_desktop_icon, get_setup_state])
 		.setup(|app| {
             // Spawn setup as a non-blocking task so the windows can be
             // created and ran while it executes
--- src/main.js
+++ src/main.js
@@ -29,6 +29,32 @@ async function fetchGameLibrary() {
     }
 }
 
+async function setup_progressbar() {
+	const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+	const progressBar = document.getElementById("progress-bar");
+	progressBar.style.width = "0%";
+	progressBar.style.transition = "none";
+	progressBar.style.display = "block";
+	progressBar.style.transition = "width 0.5s ease-in-out";
+
+	console.log("Setting up progress bar...");
+	let percent = 0;
+	let fake_percent = 0;
+	while (percent < 100) {
+		await sleep(50);
+        let state = await invoke("get_setup_state", {});
+		fake_percent = Math.min(fake_percent + Math.random() * 0.3, 100 - 5) / (state.progress || 50);
+		percent = state.progress + fake_percent;
+		progressBar.style.width = `${percent}%`;
+	}
+	percent = 100;
+	progressBar.style.width = `${percent}%`;
+	//for (let i = 0; i <= 100; i++) {
+	//	await sleep(50);
+	//	progressBar.style.width = `${i}%`;
+	//}
+}
+
 window.addEventListener("DOMContentLoaded", () => {
 	fetchGameLibrary().then(library => {
 		let i = 0;
@@ -53,7 +79,6 @@ window.addEventListener("DOMContentLoaded", () => {
 		});
 	});
 	document.getElementById("add_icon").addEventListener("click", addIcon);
+	setup_progressbar();
 });
-
-
 
