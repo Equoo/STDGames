1af03f72c0ac072529cc50731604a0c6b37aa5d4
saruM4N3 <zakobajo@gmail.com>
2025-06-30 20:56:57 +0200

    fix: enhance game card visibility and search functionality

--- src/js/ui/display.js
+++ src/js/ui/display.js
@@ -37,8 +37,8 @@ export function displayGameList(game, data, running, containerId = "game-list") 
   document.querySelector(`#${containerId}`).insertAdjacentHTML(
     "afterbegin",
     `<li class="game-list-item ${running}" id="item_${game.name}" game="${game.name}">
-      <img src="${data.icon}" alt="${game.name} icon" class="game-list-icon">
-      ${data.displayname}
+      <img src="${data.icon}" alt="${game.name} icon" class="game-list-icon"></img>
+      <h5>${data.displayname}</h5>
     </li>`
   );
 }
