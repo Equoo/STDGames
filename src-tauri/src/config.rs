d14d9840c5d8f84431981b354d394e628d71b01e
Timeo Daclin <tdaclin@student.42lyon.fr>
2025-08-11 04:28:29 +0200

    add a Config class and started doing the copy of the tools need to clean the ressources folder in the sgoinfre

---
+++ src-tauri/src/config.rs
@@ -0,0 +1,29 @@
+use std::error::Error;
+use std::env;
+
+pub struct Config {
+    pub user_save_dir: String,
+    pub resources_dir: String,
+    pub resources_junest_home_dir: String,
+    pub resource_umu_archive_file: String,
+    pub temp_dir: String,
+    pub temp_junest_home_dir: String,
+    pub temp_umu_dir: String,
+}
+
+impl Config {
+    pub fn default() -> Result<Config, Box<dyn Error>> {
+        let username = env::var("USER")?;
+        let resources_dir = "/sgoinfre/stdgames/.ressources"; // TODO: fix the typo
+        let temp_dir = format!("/tmp/stdgames/{}", username);
+        Ok(Config {
+            user_save_dir: format!("/sgoinfre/{}/.stdgames_saves", username).to_string(),
+            resources_dir: format!("{}", resources_dir).to_string(),
+            resources_junest_home_dir: format!("{}/junest", resources_dir).to_string(),
+            resource_umu_archive_file: format!("{}/umu.zip", resources_dir).to_string(),
+            temp_dir: format!("{}", temp_dir).to_string(),
+            temp_junest_home_dir: format!("{}/junest_home", temp_dir).to_string(),
+            temp_umu_dir: format!("{}/umu", temp_dir).to_string(),
+        })
+    }
+}
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,14 @@
 mod check_authorized;
+mod config;
 mod errors;
 
-use check_authorized::is_authorized;
-use errors::AppError;
+use crate::check_authorized::is_authorized;
+use crate::errors::AppError;
 
 use std::error::Error;
 use tauri::{Manager, Emitter};
 use tauri_plugin_dialog::{DialogExt, MessageDialogKind};
+use std::fs;
 
 
 fn center_window(window: &tauri::WebviewWindow) -> Result<(), Box<dyn Error>> {
@@ -21,16 +23,50 @@ fn center_window(window: &tauri::WebviewWindow) -> Result<(), Box<dyn Error>> {
 }
 
 
-use std::thread;
-use std::time::Duration;
-// setup error return in this function
-async fn setup_tools(app: tauri::AppHandle)
-{
-	loop {
-		app.emit("progressbar_update", 42);
-		thread::sleep(Duration::new(3, 0));
-		app.emit("progressbar_update", 84);
-		thread::sleep(Duration::new(3, 0));
+fn setup_tools(app: tauri::AppHandle) -> Result<(), Box<dyn Error>> {
+	println!("Installing tools ...");
+
+	app.emit("progressbar_update", 0)?;
+
+	let config = config::Config::default()?;
+
+	for directory in vec![config.resources_junest_home_dir.clone(), config.temp_junest_home_dir.clone()] {
+		// if !std::path::Path::new(&directory).exists() {
+			fs::create_dir_all(directory)?;
+		// }
+	}
+
+	let option = fs_extra::dir::CopyOptions::new().skip_exist(true);
+	// use copy_items_with_progress instead
+	// this copy as a lot of exclude, just delete the folder on the src folder
+	// this will copy to config.temp_dir/junest, maybe use option.copy_inside
+	fs_extra::copy_items(&vec![config.resources_junest_home_dir.clone()], config.temp_dir.clone(), &option)?;
+
+
+	app.emit("progressbar_update", 60)?;
+
+	tar::Archive::new(
+		fs::File::open(config.resource_umu_archive_file.clone())?)
+		.unpack(config.temp_dir.clone())?;
+	
+	app.emit("progressbar_update", 100)?;
+
+	let splash_window = app.get_webview_window("splashscreen").unwrap();
+	let main_window = app.get_webview_window("main").unwrap();
+	splash_window.close().unwrap();
+	main_window.show().unwrap();
+
+	Ok(())
+}
+
+
+async fn setup_tools_wrapper(app: tauri::AppHandle) {
+	if let Err(e) = setup_tools(app.clone()) {
+		app.dialog()
+			.message(format!("setup_tools failed: {}", e))
+			.title("Error")
+			.kind(MessageDialogKind::Error)
+			.show(|_| std::process::exit(1));
 	}
 }
 
@@ -56,7 +92,7 @@ fn setup_app(app: &mut tauri::App) -> Result<(), Box<dyn Error>> {
 	window.show()?;
 
 	// maybe use spawn_blocking instead, if there is lag maybe it's because of that
-	tauri::async_runtime::spawn(setup_tools(app.handle().clone()));
+	tauri::async_runtime::spawn(setup_tools_wrapper(app.handle().clone()));
 
 	Ok(())
 }
