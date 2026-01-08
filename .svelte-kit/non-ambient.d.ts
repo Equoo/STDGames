68617ece4c7818d8a01c7febced24d4fcc4dac70
dderny <david.derny@ik.me>
2026-01-08 03:26:57 +0000

    Front

--- .svelte-kit/non-ambient.d.ts
+++ .svelte-kit/non-ambient.d.ts
@@ -27,14 +27,15 @@ export {};
 
 declare module "$app/types" {
 	export interface AppTypes {
-		RouteId(): "/";
+		RouteId(): "/" | "/splashscreen";
 		RouteParams(): {
 			
 		};
 		LayoutParams(): {
-			"/": Record<string, never>
+			"/": Record<string, never>;
+			"/splashscreen": Record<string, never>
 		};
-		Pathname(): "/";
+		Pathname(): "/" | "/splashscreen" | "/splashscreen/";
 		ResolvedPathname(): `${"" | `/${string}`}${ReturnType<AppTypes['Pathname']>}`;
 		Asset(): string & {};
 	}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit feu <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import logo from '$lib/assets/icons/stdgames.png';
+	import discord_logo from '$lib/assets/icons/discord_100x100.png';
+
+	let ffg = "ggfg";
+</script>
+
+<div class="topbar">
+	<div class="topbar-content">
+		<div class="left-group">
+			<img src={logo} alt="Logo" class="logo">
+			<button id="addicon-button">Add to desktop</button>
+			<button id="library-button" class="active">Library</button>
+		</div>
+
+		<div class="right-group">
+			<button id="aboutus-button">
+				<img src={discord_logo} alt="Discord">
+			</button>
+			<button id="settings-button">
+				<img src="/resources/icons/settings_white.png" alt="Settings">
+			</button>
+		</div>
+	</div>
+</div>
