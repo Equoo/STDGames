47f874a0a221570381ec89007a64d4c1db71be23
dderny <david.derny@ik.me>
2026-01-23 02:32:28 +0000

    Remove build files

--- .svelte-kit/output/server/entries/pages/splashscreen/_page.svelte.js
+++
@@ -1,9 +0,0 @@
-import "clsx";
-function _page($$renderer, $$props) {
-  $$renderer.component(($$renderer2) => {
-    $$renderer2.push(`<div id="background" class="svelte-bsq5og"></div> <div class="progress svelte-bsq5og"><div class="progress-bar svelte-bsq5og"></div> <p id="progress-text"></p></div>`);
-  });
-}
-export {
-  _page as default
-};
--- .svelte-kit/output/server/nodes/3.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 3;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/splashscreen/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/3.CE8uB42S.js","_app/immutable/chunks/PPVm8Dsz.js","_app/immutable/chunks/CwhMuEnY.js","_app/immutable/chunks/wuoRkyL-.js","_app/immutable/chunks/Cas-dNfL.js","_app/immutable/chunks/BTakXyPK.js"];
-export const stylesheets = ["_app/immutable/assets/3.D9AHa7At.css"];
-export const fonts = [];
